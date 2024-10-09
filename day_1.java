92e90f1df5e7c079abeeb4942973282a900c8c13
Arpit Gupta <arpitgupta4957@gmail.com>
2024-10-09 12:29:25 +0530

    git commit

--- day_1.java
+++ day_1.java
@@ -1,5 +1,19 @@
-class day_1{
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+class day_1 {
+    public int minAddToMakeValid(String s) {
+        int left = 0, right = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;  // Count an unmatched opening bracket
+            } else {
+                if (left > 0) {
+                    left--;  // Match an opening bracket
+                } else {
+                    right++;  // Count an unmatched closing bracket
+                }
+            }
+        }
+
+        return left + right;  // Sum of unmatched left and right brackets
     }
 }
