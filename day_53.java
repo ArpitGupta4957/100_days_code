f27ca5af90826d05cbebaa498241f18ca0c8f775
Arpit Gupta <arpitgupta4957@gmail.com>
2024-12-02 22:26:21 +0530

    day_53

---
+++ day_53.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        int i=0;
+        for(String str : sentence.split(" ")){
+            if(str.startsWith(searchWord)){                
+                return i+1;
+            }
+            i++;
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.isPrefixOfWord("i love eating burger", "burg"));
+    }
+}
